3f78aa3aa0cc475417cfbc15dcb2f0e8b20e9f83
Ian Wafula <neocorpse@proton.me>
2025-04-17 05:03:59 +0300

    Add tests

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -3,7 +3,16 @@ import { currentAgeForBirthYear } from "../utils";
 describe("currentAgeForBirthYear", () => {
   it("returns the age of a person based on the year of birth", () => {
     const birthYear = 1984;
+    const currentYear = new Date().getFullYear()
+    const answer = currentYear- birthYear
     const ageOfPerson = currentAgeForBirthYear(birthYear);
-    expect(ageOfPerson).toBe(38);
+    expect(ageOfPerson).toBe(answer);
+  });
+  it("returns the current year for a person born in year 0", () => {
+    const birthYear = 0;
+    const currentYear = new Date().getFullYear()
+    const answer = currentYear - birthYear
+    const ageOfPerson = currentAgeForBirthYear(birthYear);
+    expect(ageOfPerson).toBe(answer);
   });
 });
